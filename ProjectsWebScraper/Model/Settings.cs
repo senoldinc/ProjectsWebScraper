9bd233c0b51c21f6c7852b27abeb6b186b76cd24
Senol Dinc <senol.dinc@WINDOWSLIVE.COM>
2022-01-18 21:29:40 +0300

    Configure to console app. Added to appsettings.json file . Create model fiile and scheman for settings. We are read to settings from the appsettings.json file and start to scrapping programming.

---
+++ ProjectsWebScraper/Model/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsWebScraper.Model
+{
+    public class Settings
+    {
+        public string WebSite { get; set; }
+        public string SavePath { get; set; }
+    }
+}
--- ProjectsWebScraper/Program.cs
+++ ProjectsWebScraper/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using ProjectsWebScraper.Model;
+
+// Build a config object, using env vars and JSON providers.
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Get values from the config given their key and their target type.
+Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+// Write the values to the console.
+//Console.WriteLine($"WebSite = {settings.WebSite}");
+//Console.WriteLine($"SavePath = {settings.SavePath}");
+
+ Console.WriteLine($"Hi there !!! Let's start to download {settings.WebSite} structure and save to; '{settings.SavePath}' path.");
+ Console.WriteLine("If you want to start, please type in any key.");
+
+ var anyKey = Console.ReadLine();
+
